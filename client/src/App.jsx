092e6a2c4ff7d4bbeffdd33409bfd228e505077d
Giorgos <giorgosmintis@gmail.com>
2024-10-15 21:16:15 +0300

    Added Skill Section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import HeroSection from "./sections/HeroSection/HeroSection.jsx";
 import SummarySection from "./sections/SummarySection/SummarySection.jsx";
+import SkillSection from "./sections/SkillSection/SkillSection.jsx";
 
 function App() {
   return (
     <>
       <HeroSection />
       <SummarySection />
+      <SkillSection />
     </>
   )
 }
