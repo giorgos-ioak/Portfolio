67395f25b451abd7b9bf2f73cc15a4cbdbbdc4c8
Giorgos <giorgosmintis@gmail.com>
2024-10-18 17:27:41 +0300

    Added Achievements Section

---
+++ client/src/components/Achievement/Achievement.jsx
@@ -0,0 +1,16 @@
+import Button from '../Button/Button.jsx';
+import classes from './Achievement.module.css'; 
+
+function Achievement({ label, text }) {
+  return (
+    <div className={classes.mainContainer}>
+      <label className={classes.label}>{label}</label>
+      <p className={classes.p}>{text}</p>
+      <div>
+        <Button className={'detailsBtn'}>Details</Button>
+      </div>
+    </div>
+  )
+}
+
+export default Achievement
