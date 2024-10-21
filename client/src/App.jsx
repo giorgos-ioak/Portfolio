32af0f1d20134031c510bc9429bd6a9aad5e0f26
Giorgos <giorgosmintis@gmail.com>
2024-10-21 16:30:25 +0300

    Added GetInTouch Section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import SummarySection from "./sections/SummarySection/SummarySection.jsx";
 import SkillSection from "./sections/SkillSection/SkillSection.jsx";
 import ProjectSection from "./sections/ProjectsSection/ProjectSection.jsx";
 import AchievementsSection from "./sections/AchievementsSection/AchievementsSection.jsx";
+import GetInTouchSection from "./sections/GetInTouchSection/GetInTouchSection.jsx";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <SkillSection />
       <ProjectSection />
       <AchievementsSection />
+      <GetInTouchSection />
     </>
   )
 }
