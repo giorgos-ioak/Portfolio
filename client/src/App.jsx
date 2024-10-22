1eaca3eaef575a1762d09cb69684f0c083142412
Giorgos <giorgosmintis@gmail.com>
2024-10-22 19:18:07 +0300

    Added Navbar Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,12 @@ import SkillSection from "./sections/SkillSection/SkillSection.jsx";
 import ProjectSection from "./sections/ProjectsSection/ProjectSection.jsx";
 import AchievementsSection from "./sections/AchievementsSection/AchievementsSection.jsx";
 import GetInTouchSection from "./sections/GetInTouchSection/GetInTouchSection.jsx";
+import Navbar from "./components/Navbar/Navbar.jsx";
 
 function App() {
   return (
     <>
+      <Navbar />
       <HeroSection />
       <SummarySection />
       <SkillSection />
