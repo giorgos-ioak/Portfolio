9b523af886eefa242da7660941b16c77b427168a
Giorgos <giorgosmintis@gmail.com>
2024-10-23 13:36:07 +0300

    Final page changes

--- client/src/sections/GetInTouchSection/GetInTouchSection.jsx
+++ client/src/sections/GetInTouchSection/GetInTouchSection.jsx
@@ -1,29 +1,25 @@
 import classes from './GetInTouchSection.module.css';
-// import messageIcon from '../../assets/messageIcon.svg';
+import { useMediaQuery } from '@mui/material';
 
-// import TitleContainer from '../../components/TitleContainer/TitleContainer.jsx';
 import MainContainer from '../../components/MainContainer/MainContainer.jsx';
 import SubContainer from '../../components/SubContainer/SubContainer.jsx';
 import GetInTouchContainer from '../../components/GetInTouchContainer/GetInTouchContainer.jsx';
 import RedBlock from '../../components/RedBlock/RedBlock.jsx';
 
 function GetInTouchSection() {
+  const verySmallScreen = useMediaQuery('(max-width:370px)');
+
   return (
     <section className={classes.getInTouchSection}>
-      {/* <TitleContainer
-        title='Get In Touch'
-        image={messageIcon}
-        alt='messsageIcon'
-        className='titleContainer'
-      /> */}
-
       <MainContainer className='mainContainer_GetInTouchSection'>
         <SubContainer className='subContainer_GetInTouchSection'>
           <GetInTouchContainer />
           <p className={classes.p}>Copyright 2024 © All Rights Reserved</p>
         </SubContainer>
 
-        <RedBlock className='redBlock_GetInTouchSection'/>
+        {!verySmallScreen && 
+          <RedBlock className='redBlock_GetInTouchSection'/>
+        }
       </MainContainer>
     </section>
   )
