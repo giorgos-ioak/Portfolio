62521d6f7ec1fffd4a79f7e986cd1c4b8dc8144e
Giorgos <giorgosmintis@gmail.com>
2024-11-07 16:05:28 +0200

    Added projects page

---
+++ client/src/components/Layouts/MainLayout.jsx
@@ -0,0 +1,55 @@
+import { Outlet } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
+import { storeData } from "../../app/reducers/databaseData.js";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+
+import Navbar from "../Navbar/Navbar.jsx";
+
+
+
+
+function MainLayout() {
+  const data = useLoaderData();
+  const dispatch = useDispatch();
+
+
+  useEffect(() => {
+    dispatch(storeData(data));
+    // console.log('UseEffect executed');
+  }, [dispatch, data]);
+
+
+
+
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  )
+};
+
+export default MainLayout;
+
+
+
+
+
+
+
+export const loader = async() => {
+  try {
+    const response = await fetch('http://localhost:3000/dbData');
+  
+    if(!response.ok) {
+      throw new Error(`There was an HTTP error with a status of: ${response.status}.`);
+    }
+
+    const data = await response.json();
+    return data;
+
+  } catch(err) {
+    return {error: true,message: err.message};
+  }
+};
