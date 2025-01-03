bcb698718d11ecd31db24602f15eaee3bf4435f8
Giorgos <giorgosmintis@gmail.com>
2025-01-03 14:51:46 +0200

    Chnages

--- server/server.js
+++ server/server.js
@@ -58,8 +58,9 @@ app.use('/', dataRoutes);
 app.use('/auth', authRoutes);
 
 
+const PORT = process.env.PORT || 3000;
 
 // START SERVER
-app.listen(3000, () => {
-  console.log("Server started successfully");
+app.listen(PORT, () => {
+  console.log("Server started successfully ");
 });
