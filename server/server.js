d88aa5da2d2a40408369576b0c0de3a5be2a7b26
Giorgos <giorgosmintis@gmail.com>
2025-01-05 17:49:09 +0200

    Added url to cors

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ dotenv.config();
 
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://portfolio-frontend-t0qg.onrender.com', credentials: true }));
 app.use('/uploads', express.static('uploads'));
 app.use(cookieParser());
 app.use(express.json());
