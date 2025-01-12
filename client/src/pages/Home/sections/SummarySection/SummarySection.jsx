6f0b3489927aba9ac8d3705cef6b6e749604c8cb
Giorgos <giorgosmintis@gmail.com>
2025-01-12 15:14:13 +0200

    Edited Summary section

--- client/src/pages/Home/sections/SummarySection/SummarySection.jsx
+++ client/src/pages/Home/sections/SummarySection/SummarySection.jsx
@@ -17,7 +17,7 @@ function SummarySection() {
         I am a 24 year old highly motivated and detail-oriented <strong>Full-Stack Software Developer</strong> based in Thessaloniki, Greece.<br/>I have experience designing and building dynamic, responsive, and user-friendly web applications.<br/><br/>
 
         My technical expertise includes JavaScript, C#, React.js, Node.js, Express.js, and .NET Framework.
-        <br/><br/>My GitHub portfolio showcases a diverse range of projects that demonstrate my ability to develop robust <strong>full stack</strong> applications. I stay updated with new technologies to create effective and reliable digital solutions for clients.
+        <br/><br/>My GitHub portfolio showcases a diverse range of projects that demonstrate my ability to develop <strong>full stack</strong> applications. I stay updated with new technologies to create effective and reliable digital solutions for clients.
 
 
 
